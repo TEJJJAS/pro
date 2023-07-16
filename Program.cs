f090f0e74e7a1e69d80b94ff4426bc670faff23b
TEJJJAS <134104418+TEJJJAS@users.noreply.github.com>
2023-07-16 14:30:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Students> lststu = new List<Students>
+            {
+                new Students { stuid = 1 , name ="tejas sonawane" , marks =80},
+            new Students { stuid = 1, name = "tushar patil ", marks = 90 },
+            new Students { stuid = 1, name = "tejas chaudhari", marks = 60 }
+
+            };
+
+            IEnumerable<Students> lst = from Students in lststu where Students.marks==80 select Students;
+
+            foreach (Students sdt in lst)
+            {
+                Console.WriteLine(sdt.stuid + " "+sdt.name+" "+sdt.marks);
+
+            }
+        }
+    }
+
+    internal class Students
+    {
+        public int stuid;
+        public string name;
+        public int marks;
+
+    }
+}
